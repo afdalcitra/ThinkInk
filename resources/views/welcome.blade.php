87e0a164cb5a1dba2d302dd9bf03f9158b45c20b
afdalcitra <afdalcitra.edu@gmail.com>
2025-03-27 14:07:14 +0700

    Final Changes!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,12 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-        @vite('resources/css/app.css')
+        @vite('resources/css/app.css', ['https' => true])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <div class="text-center">
             <h1 class="text-4xl lg:text-6xl font-bold text-purple-500">Welcome to Laravel</h1>
-            <p class="text-lg lg:text-2xl mt-4">The PHP Framework For Web Artisans</p>
+            <p class="text-lg lg:text-2xl mt-4 text-white">The PHP Framework For Web Artisans</p>
         </div>
     </body>
 </html>
