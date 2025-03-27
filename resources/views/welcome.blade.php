db8e9eacc55dcba7ef14b8e56f7b7c9f763a9914
afdalcitra <afdalcitra.edu@gmail.com>
2025-03-27 13:48:44 +0700

    asda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,11 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-        <link href="/src/styles.css" rel="stylesheet">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <div class="text-center">
-            <h1 class="text-4xl lg:text-6xl font-bold">Welcome to Laravel</h1>
+            <h1 class="text-4xl lg:text-6xl font-bold text-purple-500">Welcome to Laravel</h1>
             <p class="text-lg lg:text-2xl mt-4">The PHP Framework For Web Artisans</p>
         </div>
     </body>
